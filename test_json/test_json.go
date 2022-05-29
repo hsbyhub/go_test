d6136d0d3d99b5666fffd35e532c1fe8223a4b43
xiesenxin <xiesenxin@bytedance.com>
2022-05-29 11:45:00 +0800

    添加

--- test_json/test_json.go
+++ test_json/test_json.go
@@ -1,48 +1,60 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
+	"encoding/json"
+	"fmt"
 )
 
 type Student struct {
-    Name    string  `json:"StuName"`
-    Age     int     `json:"age"`
-    Sex     int     `json:"sex"`
+	Name string `json:"StuName"`
+	Age  int    `json:"age"`
+	Sex  int    `json:"sex"`
 }
 
 func testPointStruct() {
-    //指针变量
-    stu := &Student {
-        Name: "mike", 
-        Age: 19,
-        Sex: 0,
-    }
-    //Marshal失败时err!=nil
-    jsonStu, err := json.Marshal(*stu)
-    if err != nil {
-        fmt.Println("生成json字符串错误")
-    }
+	//指针变量
+	stu := &Student{
+		Name: "mike",
+		Age:  19,
+		Sex:  0,
+	}
+	//Marshal失败时err!=nil
+	jsonStu, err := json.Marshal(*stu)
+	if err != nil {
+		fmt.Println("生成json字符串错误")
+	}
 
-    //jsonStu是[]byte类型，转化成string类型便于查看
-    fmt.Println(string(jsonStu))
+	//jsonStu是[]byte类型，转化成string类型便于查看
+	fmt.Println(string(jsonStu))
 
 }
 
-func TestMapToJson() {
-    var mp = map[string]map[string]interface{}{
-        "A" : map[string]interface{}{"a" : 1},
-        "B" : map[string]interface{}{"a" : 1},
-        "C" : map[string]interface{}{"a" : 1},
-    }
-    mp_json, err := json.Marshal(mp)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    fmt.Println(string(mp_json))
+func testMapToJson() {
+	paramsJsonStr := `{
+			"domain": "video01.dgsltx.com",
+			"ts": 1652842800,
+			"regions": null,
+			"isps": null,
+			"provs": null,
+			"nodes": null
+		}`
+	var params map[string]interface{}
+	err := json.Unmarshal([]byte(paramsJsonStr), &params)
+	if err != nil {
+		fmt.Println("json.Unmarshal error [%v]", err)
+		return
+	}
+	fmt.Println("json.Unmarshal result [%v]", params)
+
+	var resultJson []byte
+	resultJson, err = json.Marshal(params)
+	if err != nil {
+		fmt.Println("json.Marshal error [%v]", err)
+		return
+	}
+	fmt.Println("json.Marshal return [%v]", string(resultJson))
 }
 
 func main() {
-    testPointStruct()
+	testMapToJson()
 }
